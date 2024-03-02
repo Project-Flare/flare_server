3640ec1ac62a7b3913eed74c23fbed1b35585675
pine-forest-dweller <159782252+herkusleonkaselis@users.noreply.github.com>
2024-03-02 21:45:13 +0200

    dev: add shared_proto submodule

--- src/main.c
+++ src/main.c
@@ -7,19 +7,21 @@ void fn(struct mg_connection *c, int ev, void *ev_data) {
     if (ev == MG_EV_HTTP_MSG) {
         // upgrade all of http to ws
         struct mg_http_message *hm = (struct mg_http_message *) ev_data;
-        mg_http_reply(c, 200, "", "flare_server\n");    // Yes. Respond JSON
+        mg_http_reply(c, 200, "", "flare_server\n", "%s");
         mg_ws_upgrade(c, hm, NULL);
     } else if (ev == MG_EV_WS_MSG) {
         // got websocket frame, rec data in wm->data. echo.
         struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;
         mg_ws_send(c, wm->data.ptr, wm->data.len, WEBSOCKET_OP_TEXT);
+        
+        
     }
 }
 
 int main(void) {
     struct mg_mgr mgr;  // event mgr
     mg_mgr_init(&mgr);  // init event mgr
-    printf("Starting WS listener on %s/websocket\n", s_listen_on);
+    printf("Starting WS listener");
     mg_http_listen(&mgr, s_listen_on, fn, NULL);  // http listener bound to fn
     for (;;) mg_mgr_poll(&mgr, 1000); // inf loop
     mg_mgr_free(&mgr);
