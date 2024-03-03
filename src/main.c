74567c000ff3312fd5c6598796f2d0d7c4b38d3a
pine-forest-dweller <159782252+herkusleonkaselis@users.noreply.github.com>
2024-03-04 01:40:02 +0200

    dev: bring in ws upgrading and echoing

--- src/main.c
+++ src/main.c
@@ -19,71 +19,17 @@ static const char *s_key_path = "./flare_data/key.pem";
 struct mg_tls_opts opts;
 
 static void fn(struct mg_connection *c, int ev, void *ev_data) {
-  if (ev == MG_EV_ACCEPT && c->fn_data != NULL) {
-    mg_tls_init(c, &opts);
-  }
-  if (ev == MG_EV_HTTP_MSG) {
-    struct mg_http_message *hm = (struct mg_http_message *) ev_data;
-    if (mg_http_match_uri(hm, "/api/stats")) {
-      // Print some statistics about currently established connections
-      mg_printf(c, "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
-      mg_http_printf_chunk(c, "ID PROTO TYPE      LOCAL           REMOTE\n");
-      for (struct mg_connection *t = c->mgr->conns; t != NULL; t = t->next) {
-        mg_http_printf_chunk(c, "%-3lu %4s %s %M %M\n", t->id,
-                             t->is_udp ? "UDP" : "TCP",
-                             t->is_listening  ? "LISTENING"
-                             : t->is_accepted ? "ACCEPTED "
-                                              : "CONNECTED",
-                             mg_print_ip, &t->loc, mg_print_ip, &t->rem);
-      }
-      mg_http_printf_chunk(c, "");  // Don't forget the last empty chunk
-    } else if (mg_http_match_uri(hm, "/api/f2/*")) {
-      mg_http_reply(c, 200, "", "{\"result\": \"%.*s\"}\n", (int) hm->uri.len,
-                    hm->uri.ptr);
+    if (ev == MG_EV_ACCEPT && c->fn_data != NULL) {
+        mg_tls_init(c, &opts);
+    } else if (ev == MG_EV_HTTP_MSG) {
+        struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+        mg_ws_upgrade(c, hm, NULL);
+    } else if (ev == MG_EV_WS_MSG) {
+        // got websocket frame, rec data in wm->data. echo.
+        struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;
+        mg_ws_send(c, wm->data.ptr, wm->data.len, WEBSOCKET_OP_TEXT);
     }
-  }
 }
-
-// static void fn(struct mg_connection *c, int ev, void *ev_data) {
-//     if (ev == MG_EV_ACCEPT && mg_url_is_ssl(s_listen_on)) {
-//         struct mg_str cert_data = mg_file_read(&mg_fs_posix, s_cert_path);
-//         struct mg_str privkey_data = mg_file_read(&mg_fs_posix, s_key_path);
-//         struct mg_tls_opts ops = { .cert = cert_data, .key = privkey_data };
-
-//         mg_tls_init(c, &ops);
-
-//         // free((void *) cert_data.ptr);
-//         // free((void *) privkey_data.ptr);
-//     } else if (ev == MG_EV_TLS_HS) {
-//         MG_INFO(("TLS handshake done! Sending EHLO again"));
-//         mg_printf(c, "EHLO myname\r\n");
-//     } else if (ev == MG_EV_HTTP_MSG) {
-//         mg_http_reply(c, 200, "Content-Type: text/plain\r\n", "%s\n", "hello from flare");
-//     }
-
-//     // if (ev == MG_EV_OPEN) {
-//     //     printf("open conn.");
-//     // } else if (ev == MG_EV_READ) {
-//     // }
-    
-//     // if (ev == MG_EV_HTTP_MSG) {
-//     //     struct mg_http_message *hm = (struct mg_http_message *) ev_data;
-//     //     if (mg_http_match_uri(hm, "/ws")) {
-//     //         mg_ws_upgrade(c, hm, NULL);
-//     //     } else if (mg_http_match_uri(hm, "/test")) {
-//     //         struct mg_http_serve_opts opts = {
-//     //             .mime_types = "html=text/html",
-//     //         };
-//     //         mg_http_serve_file(c, ev_data, "./flare_data/test.html", &opts);
-//     //     } else {
-//     //         mg_http_reply(c, 200, "Content-Type: text/plain\r\n", "%s\n", "hello from flare");
-//     //     }
-//     // } else if (ev == MG_EV_WS_MSG) {
-//     //     // got websocket frame, rec data in wm->data. echo.
-//     //     struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;
-//     //     mg_ws_send(c, wm->data.ptr, wm->data.len, WEBSOCKET_OP_TEXT);
-//     // }
-// }
 
 char* concat(const char *str_1, const char *str_2)
 {
